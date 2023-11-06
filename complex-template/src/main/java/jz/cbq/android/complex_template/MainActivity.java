75dfab71ea878203281e2182a48d5e7a0e1a4c2d
cola777jz <cola777jz@gmail.com>
2023-11-06 20:38:58 +0800

    bottom and toolbar

--- complex-template/src/main/java/jz/cbq/android/complex_template/MainActivity.java
+++ complex-template/src/main/java/jz/cbq/android/complex_template/MainActivity.java
@@ -2,12 +2,91 @@ package jz.cbq.android.complex_template;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.fragment.app.FragmentTransaction;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import jz.cbq.android.complex_template.fragment.CarFragment;
+import jz.cbq.android.complex_template.fragment.HomeFragment;
+import jz.cbq.android.complex_template.fragment.MineFragment;
+import jz.cbq.android.complex_template.fragment.OrderFragment;
 
 public class MainActivity extends AppCompatActivity {
+    private HomeFragment home;
+    private CarFragment car;
+    private OrderFragment order;
+    private MineFragment mine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottom = findViewById(R.id.bottomNavigationView);
+
+        selectedFragment(0);
+
+        bottom.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.home) {
+                selectedFragment(0);
+            } else if (item.getItemId() == R.id.car) {
+                selectedFragment(1);
+            } else if (item.getItemId() == R.id.order) {
+                selectedFragment(2);
+            } else {
+                selectedFragment(3);
+            }
+            return true;
+        });
+    }
+
+    private void selectedFragment(int position) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        hideFragment(transaction);
+
+        if (position == 0) {
+            if (home == null) {
+                home = new HomeFragment();
+                transaction.add(R.id.content, home);
+            } else {
+                transaction.show(home);
+            }
+        } else if (position == 1) {
+            if (car == null) {
+                car = new CarFragment();
+                transaction.add(R.id.content, car);
+            } else {
+                transaction.show(car);
+            }
+        } else if (position == 2) {
+            if (order == null) {
+                order = new OrderFragment();
+                transaction.add(R.id.content, order);
+            } else {
+                transaction.show(order);
+            }
+        } else {
+            if (mine == null) {
+                mine = new MineFragment();
+                transaction.add(R.id.content, mine);
+            } else {
+                transaction.show(mine);
+            }
+        }
+
+        transaction.commit();
+    }
+
+    private void hideFragment(FragmentTransaction transaction) {
+        if (home != null) {
+            transaction.hide(home);
+        }
+        if (car != null) {
+            transaction.hide(car);
+        }
+        if (order != null) {
+            transaction.hide(order);
+        }
+        if (mine != null) {
+            transaction.hide(mine);
+        }
     }
 }
---
+++ complex-template/src/main/java/jz/cbq/android/complex_template/fragment/HomeFragment.java
@@ -0,0 +1,18 @@
+package jz.cbq.android.complex_template.fragment;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import jz.cbq.android.complex_template.R;
+
+public class HomeFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
